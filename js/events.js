93930c5f976533845831bd4349d23584c56e328d
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-31 18:58:01 +0600

    Add some onclick button and click button than change background color for full page

--- js/events.js
+++ js/events.js
@@ -1,1 +1,14 @@
 console.log('This is separte js file.');
+
+// option-2: add onclick function
+function makeRed(){
+    document.body.style.backgroundColor='red';
+}
+
+// option-3: 
+const makeBlueButton=document.getElementById('make-blue');
+        makeBlueButton.onclick=makeBlue;
+
+function makeBlue(){
+    document.body.style.backgroundColor='blue';
+}
